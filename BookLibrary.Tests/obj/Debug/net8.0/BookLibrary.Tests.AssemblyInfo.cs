7951f931477f194cfe2b1968f2f8e3f63048be7e
Mohammad Anouti <manouti1988@gmail.com>
2024-12-18 20:10:52 +0200

    test docker image fix for unit test

--- BookLibrary.Tests/obj/Debug/net8.0/BookLibrary.Tests.AssemblyInfo.cs
+++ BookLibrary.Tests/obj/Debug/net8.0/BookLibrary.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookLibrary.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0cb70b972ad1b6d510035fb791b70d7ca26b24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+498b8f8181daad86ddd8a792f0f30e0907758e4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookLibrary.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookLibrary.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
